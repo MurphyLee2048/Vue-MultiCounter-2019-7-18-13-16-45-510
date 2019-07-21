28f3197baf8489e8a0958babc0057430b155525a
murphylee2048 <1134697131@qq.com>
2019-07-21 16:16:51 +0800

    feat: Counter

---
+++ vue-counter/src/components/Counter.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <button v-on:click="decrease">-</button>
+    <span>{{count}}</span>
+    <button v-on:click="increase">+</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Counter",
+    methods: {
+      increase() {
+        this.count++;
+      },
+      decrease() {
+        this.count--;
+      }
+    },
+    data() {
+      return {
+        count: 0
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue-counter/src/components/CounterGroup.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <counter v-for+="n in counternumber"></counter>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "CounterGroup",
+    props: {
+
+      counterNum: Number
+    },
+    beforeDestroy: function () {
+      this.$emit('destory', this.count);
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
